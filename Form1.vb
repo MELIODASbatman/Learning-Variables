4ab909769adcf2f04c5567023240749cb5c17699
MELIODASbatman <brianjoker197@gmail.com>
2026-08-21 09:03:10 +0300

    created a commenta for each part of a code

--- Form1.vb
+++ Form1.vb
@@ -4,12 +4,15 @@
     End Sub
 
     Private Sub btnregister_Click(sender As Object, e As EventArgs) Handles btnregister.Click
+        'creating variables to store the values entered by the user
         Dim studentName As String
         Dim Age As Integer
         Dim Course As String
+        'intializing the variables with the values entered by the user
         studentName = txtname.Text
         Age = txtage.Text
         Course = txtcourse.Text
+        'writing the message box to display the values entered by the user
         MessageBox.Show("Name: " & studentName & vbCrLf & "Age: " & Age & vbCrLf & "Course: " & Course)
     End Sub
 End Class
